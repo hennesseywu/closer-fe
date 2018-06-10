a811f810e52624cae90e12a95221b3a9b7c25e6f
hennessey <yuhang.wu@tiejin.com>
2018-06-10 15:04:42 +0800

    修改测试 ios登录

--- src/router.js
+++ src/router.js
@@ -13,7 +13,23 @@ const Share = () =>
 
 Vue.use(Router)
 
-
+setupWebViewJavascriptBridge = function(callback) {
+    // console.log(ca llback)
+    if (window.WebViewJavascriptBridge) {
+        return callback(WebViewJavascriptBridge);
+    }
+    if (window.WVJBCallbacks) {
+        return window.WVJBCallbacks.push(callback);
+    }
+    window.WVJBCallbacks = [callback];
+    var WVJBIframe = document.createElement('iframe');
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'https://__bridge_loaded__';
+    document.documentElement.appendChild(WVJBIframe);
+    setTimeout(function() {
+        document.documentElement.removeChild(WVJBIframe)
+    }, 0)
+}
 
 const router = new Router({
 
