f5094114c079558d08a314819e3c307e896a7b27
hennessey <yuhang.wu@tiejin.com>
2018-06-11 14:07:11 +0800

    修改请求closer_agent传参

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -9,7 +9,8 @@ export function redirectAddChance(isApp) {
 
 export function downloadApp() {
     console.log("downloadApp");
-    let ua = navigator.userAgent.toLowerCase() || window.navigator.userAgent.toLowerCase();
+    let ua = navigator.userAgent || window.navigator.userAgent;
+    ua = ua.toLowerCase();
     if (ua.indexOf('ipad') > -1 || ua.indexOf('iphone') > -1) {
         location.href = "https://itunes.apple.com/us/app/%E8%B4%B4%E8%BF%91/id1363128511?l=zh&ls=1&mt=8";
     } else if (ua.indexOf('android')) {
