31c35a1cf484c9575bee2e1a686463e2a17d8e07
hennessey <yuhang.wu@tiejin.com>
2018-06-11 16:14:32 +0800

    修改获取机会以后刷新

--- src/pages/worldcup/activity/module.js
+++ src/pages/worldcup/activity/module.js
@@ -66,7 +66,9 @@ export default {
                 //     "count": 11, //-----这次获取的次数
                 //     "status": true //-----是否领取成功
                 // }
-                commit({ type: 'updateRecieveChance', result })
+                commit({ type: 'updateRecieveChance', result });
+                await dispatch('getUserGuessStatistic');
+
             } else {
                 Toast('网络开小差啦，请稍后再试')
             }
