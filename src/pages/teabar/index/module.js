658bf23f99f334da18b78f4a6e72e9c875ae06c1
hennessey <yuhang.wu@tiejin.com>
2018-07-09 14:06:59 +0800

    饮水机活动修改ios登录

--- src/pages/teabar/index/module.js
+++ src/pages/teabar/index/module.js
@@ -70,39 +70,42 @@ export default {
             let ua = rootState.UA;
             if (ua.indexOf("closer-ios") > -1) {
                 console.log("module closer-ios");
-                setupWebViewJavascriptBridge(function(bridge) {
-                    console.log("ios bridge", bridge)
-                    if (bridge) {
-                        //ios获取用户token 判断登录
-                        bridge.callHandler("getUserToken", null, function(token, responseCallback) {
-                            console.log("ios token", token)
-                            if (token) {
-                                Cookies.set("GroukAuth", token, { expires: 7 });
-                                setTimeout(() => {
-                                    axios.post(api.admin.user_show).then(({ data }) => {
-                                        console.log("ios", data.result);
-                                        if (data.result) {
-                                            Cookies.set("user", JSON.stringify(data.result), { expires: 60 });
-                                            cb(true)
-                                        } else {
-                                            cb();
-                                        }
-                                    }).catch(err => {
-                                        Toast('网络开小差啦，请稍后再试')
-                                        return;
-                                    })
+                setTimeout(() => {
+                    setupWebViewJavascriptBridge(function(bridge) {
+                        console.log("ios bridge", bridge)
+                        if (bridge) {
+                            //ios获取用户token 判断登录
+                            bridge.callHandler("getUserToken", null, function(token, responseCallback) {
+                                console.log("ios token", token)
+                                if (token) {
+                                    Cookies.set("GroukAuth", token, { expires: 7 });
+                                    setTimeout(() => {
+                                        axios.post(api.admin.user_show).then(({ data }) => {
+                                            console.log("ios", data.result);
+                                            if (data.result) {
+                                                Cookies.set("user", JSON.stringify(data.result), { expires: 60 });
+                                                cb(true)
+                                            } else {
+                                                cb();
+                                            }
+                                        }).catch(err => {
+                                            Toast('网络开小差啦，请稍后再试')
+                                            return;
+                                        })
 
-                                }, 1000)
-                            } else {
-                                console.log("ios jumpLogin")
-                                setupWebViewJavascriptBridge(function(bridge) {
-                                    bridge.callHandler("jumpLogin", null);
-                                });
-                                cb();
-                            }
-                        });
-                    }
-                })
+                                    }, 1000)
+                                } else {
+                                    console.log("ios jumpLogin")
+                                    setupWebViewJavascriptBridge(function(bridge) {
+                                        bridge.callHandler("jumpLogin", null);
+                                    });
+                                    cb();
+                                }
+                            });
+                        }
+                    })
+                }, 500)
+
             } else if (ua.indexOf("closer-android") > -1) {
                 console.log("closer-android")
                 console.log("module android", typeof window.bridge != "undefined")
