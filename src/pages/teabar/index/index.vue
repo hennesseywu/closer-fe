ad2acb3b90bd80cc1d4858dda66b7672e0e4a769
hennessey <yuhang.wu@tiejin.com>
2018-07-10 16:58:05 +0800

    测试微信分享

--- src/pages/teabar/index/index.vue
+++ src/pages/teabar/index/index.vue
@@ -102,47 +102,46 @@
       }
     },
     async mounted() {
-      let wxConfig = await this.wechatConfig();
-      console.log('wxConfig', wxConfig)
-      if (wxConfig && wxConfig.signature && wxConfig.appId && wxConfig.nonceStr && wxConfig.timestamp) {
-        wx.config({
-          "debug": this.$store.state.IS_DEV, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-          "signature": wxConfig.signature,
-          "appId": wxConfig.appId,
-          "nonceStr": wxConfig.nonceStr,
-          "timestamp": wxConfig.timestamp,
-          jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表
-        });
-        wx.ready(function() {
-          console.log("ready")
-          wx.onMenuShareAppMessage({
-              title: '贴近一点，看身边', // 分享标题
-              desc: '我参加贴近免费领取一台饮水机', // 分享描述
-              link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-              imgUrl: 'https://tiejin.cn/public/img/index/tiejin_white.png', // 分享图标
-              type: '', // 分享类型,music、video或link，不填默认为link
-              dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-              success: function() {
-                // 用户点击了分享后执行的回调函数
-              }
-            })
+      // let wxConfig = await this.wechatConfig();
+      // console.log('wxConfig', wxConfig)
+      // if (wxConfig && wxConfig.signature && wxConfig.appId && wxConfig.nonceStr && wxConfig.timestamp) {
+      //   wx.config({
+      //     "debug": this.$store.state.IS_DEV, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      //     "signature": wxConfig.signature,
+      //     "appId": wxConfig.appId,
+      //     "nonceStr": wxConfig.nonceStr,
+      //     "timestamp": wxConfig.timestamp,
+      //     jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表
+      //   });
+      //   wx.ready(function() {
+      //     console.log("ready")
+      //     wx.onMenuShareAppMessage({
+      //       title: '贴近一点，看身边', // 分享标题
+      //       desc: '我参加贴近免费领取一台饮水机', // 分享描述
+      //       link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      //       imgUrl: 'https://tiejin.cn/public/img/index/tiejin_white.png', // 分享图标
+      //       type: '', // 分享类型,music、video或link，不填默认为link
+      //       dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+      //       success: function() {
+      //         // 用户点击了分享后执行的回调函数
+      //       }
+      //     })
   
-            // wx.onMenuShareTimeline({
-            //   title: '', // 分享标题
-            //   link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            //   imgUrl: '', // 分享图标
-            //   success: function() {
-            //     // 用户点击了分享后执行的回调函数
-            //   }
-            // })
-          // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-        // });
-        })
-      }
-        wx.error(function(res) {
-          console.log("error", res)
-          // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-        });
+      //     wx.onMenuShareTimeline({
+      //       title: '', // 分享标题
+      //       link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      //       imgUrl: '', // 分享图标
+      //       success: function() {
+      //         // 用户点击了分享后执行的回调函数
+      //       }
+      //     })
+      //     // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+      //   })
+      //   wx.error(function(res) {
+      //     console.log("error", res)
+      //     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+      //   });
+      // }
   
       if (this.$store.state.IS_APP) {
         this.checkLogin(async(res) => {
@@ -155,7 +154,7 @@
           }
         });
       } else {
-        // this.doWaterAction()
+        this.doWaterAction();
       }
   
     },
