3ff5e1564d2147dc3b598e2faec1453e95f36751
hennessey <yuhang.wu@tiejin.com>
2018-08-16 11:00:19 +0800

    增加图片

--- src/pages/pullnew/index/index.vue
+++ src/pages/pullnew/index/index.vue
@@ -40,30 +40,59 @@
     </div>
     <div class="bottom">
       <div class="bottom-tab">
-        <div class="tab-left" @click="clickTab('tabLeft')">新邀请好友</div>
-        <div class="tab-right" @click="clickTab('tabRight')">历史好友</div>
+        <div :class="tabActive=='tabLeft' ?'tab-left' :'tab-right'" @click="clickTab('tabLeft')">新邀请好友</div>
+        <div :class="tabActive=='tabLeft' ? 'tab-right':'tab-left'" @click="clickTab('tabRight')">历史好友</div>
       </div>
       <mt-tab-container v-model="tabActive">
         <mt-tab-container-item class="friends-list" id='tabLeft'>
           <div class="remind-title">
             好友完成邀请进度，你将获取对应邀请的奖励金额
           </div>
-          <div class="loadmore">
+          <mt-loadmore class="loadmore" v-if="!pullNewStatic.awardEnd&&loginUsers.length> 0" :bottom-method="loadBottom" :auto-fill="false" :bottom-all-loaded="allLoaded" :bottomPullText="bottomPullText" :bottomLoadingText="bottomLoadingText" :bottomDistance="bottomDistance"
+            ref="loadmore">
             <div class="remind-content">
-              <div class="new-friend">
+              <div class="new-friend" v-for="(value,key) in loginUsers" :key="key">
                 <div class="top">
-                  <img class="headphoto" src="https://file-sandbox.tiejin.cn/avatar/u/9uxNUky7dr?v=1530096892354" />
-                  <div class="nickname">惺惺相惜</div>
-                  <div class="date">2018-08-15</div>
+                  <img class="headphoto" :src="fileUrl+value.inviteeUser.avatar" />
+                  <div class="nickname">{{value.inviteeUser.fullname}}</div>
+                  <div class="date">{{formateDate(value.inviteeUser.createTime)}}</div>
                 </div>
-                <div class="middle">
-                  <div class="dot"></div>
+                <div class="middle" v-if="value.userActions&&value.userActions.userActionEnum&&value.userActions.userActionEnum=='listFeed'">
+                  <div class="dot-active"></div>
                   <div class="line"></div>
+                  <div class="dot-grey"></div>
+                  <div class="line"></div>
+                  <div class="dot-grey"></div>
                 </div>
-                <div class="bottom"></div>
+                <div class="middle" v-else-if="value.userActions&&value.userActions.userActionEnum&&value.userActions.userActionEnum=='listFeed'&&value.userActions.userActionEnum.count==1">
+                  <div class="dot-active"></div>
+                  <div class="line"></div>
+                  <div class="dot-active"></div>
+                  <div class="line"></div>
+                  <div class="dot-grey"></div>
+                </div>
+                <div class="middle" v-else-if="value.userActions&&value.userActions.userActionEnum&&value.userActions.userActionEnum=='listFeed'&&value.userActions.userActionEnum.count==2">
+                  <div class="dot-active"></div>
+                  <div class="line"></div>
+                  <div class="dot-active"></div>
+                  <div class="line"></div>
+                  <div class="dot-active"></div>
+                </div>
+                <div class="middle" else>
+                  <div class="dot-grey"></div>
+                  <div class="line"></div>
+                  <div class="dot-grey"></div>
+                  <div class="line"></div>
+                  <div class="dot-grey"></div>
+                </div>
+                <div class="progress-bottom" v-if="value.userActions&&value.userActions.userActionEnum&&value.userActions.userActionEnum=='listFeed'">
+                  <span class="desc1">进入首页</span>
+                  <span class="desc2 ">查看1篇文章</span>
+                  <span class="desc3">查看2篇文章</span>
+                </div>
               </div>
             </div>
-          </div>
+          </mt-loadmore>
         </mt-tab-container-item>
         <mt-tab-container-item class="friends-list" id='tabRight'>
           <div v-if="pullNewStatic.inviteUserTotalCount > 0">
@@ -82,8 +111,8 @@
           <div class="remind-title" v-else>
             您还没有好友哦，快去邀请好友赚钱吧！
           </div>
-          <mt-loadmore class="loadmore" v-if="!pullNewStatic.awardEnd&&loginUsers.length> 0" :bottom-method="loadBottom" :auto-fill="false" :bottom-all-loaded="allLoaded" :bottomPullText="bottomPullText" :bottomLoadingText="bottomLoadingText" :bottomDistance="bottomDistance"
-            ref="loadmore">
+          <mt-loadmore class="loadmore" v-if="!pullNewStatic.awardEnd&&loginUsers.length> 0" :bottom-method="loadBottom" :auto-fill="false" :bottom-all-loaded="allLoaded1" :bottomPullText="bottomPullText" :bottomLoadingText="bottomLoadingText" :bottomDistance="bottomDistance"
+            ref="loadmore1">
             <div class="remind-content">
               <div class="friend" v-for="(value,key) in loginUsers" :key="key">
                 <img :src="fileUrl+value.inviteeUser.avatar" class="headphoto">
@@ -93,7 +122,7 @@
                 </div>
                 <div class="amount" v-if="value.loginAmount">+{{formateMoney(value.loginAmount/100)}}</div>
                 <div v-else :class="value.reminded ? 'reminded':'remind-login'" @click="remind(value.inviteeUser.objsectID,value.reminded,$event)"></div>
-              </div>.
+              </div>
             </div>
           </mt-loadmore>
         </mt-tab-container-item>
@@ -134,6 +163,7 @@
         bottomLoadingText: '加载中',
         redbagVisiable: false,
         allLoaded: false,
+        allLoaded1: false,
         inviteUsers: [],
         loginUsers: [],
         pageNum: 1,
@@ -194,7 +224,8 @@
           } = await this.getInviteUserList({
             pagenum: this.pageNum,
             pagesize: this.pageSize,
-            "noIndicator": true
+            "noIndicator": true,
+            "awardType": "firstAward"
           });
           this.totalPageNum = Math.ceil(count / pagesize)
           this.loginUsers = data;
@@ -218,25 +249,38 @@
       ...mapActions("pullNew", ["checkLogin", "getInviteUserList", "getPullNewInfo", "remindLogin", "getYesterdayAwardAmt"]),
       async loadBottom() {
         if (this.pageNum == this.totalPageNum) {
-          this.$refs.loadmore.onBottomLoaded();
+          if (this.tabActive == 'tabLeft') {
+            this.$refs.loadmore.onBottomLoaded();
+          } else {
+            this.$refs.loadmore1.onBottomLoaded();
+          }
           this.allLoaded = true;
           this.bottomPullText = ""
           return;
         }
         this.pageNum++;
+        let type = "firstAward";
+        if (this.tabActive == "tabRight") {
+          type = "dailyAward"
+        }
         let {
           data,
           pagesize,
           count
         } = await this.getInviteUserList({
           pagenum: this.pageNum,
-          pagesize: this.pageSize
+          pagesize: this.pageSize,
+          awardType: type
         });
         this.totalPageNum = Math.ceil(count / pagesize)
         for (var a in data) {
           this.loginUsers.push(data[a]);
         }
-        this.$refs.loadmore.onBottomLoaded();
+        if (this.tabActive == 'tabLeft') {
+          this.$refs.loadmore.onBottomLoaded();
+        } else {
+          this.$refs.loadmore1.onBottomLoaded();
+        }
         if (this.pageNum == this.totalPageNum) {
           this.allLoaded = true;
           this.bottomPullText = ""
@@ -665,19 +709,46 @@
                   margin-left: 19pr;
                   width: 424pr;
                 }
-                .date {
-                }
               }
               .middle {
+                display: flex;
+                flex-direction: row;
+                margin: 17pr 102pr 17pr 102pr;
                 .dot {
-                background: url()
+                  background: url(../assets/images/dot1.png);
+                  background-size: cover;
+                  width: 33pr;
+                  height: 33pr;
                 }
-                .line{
-
+                .dot-grey {
+                  background: url(../assets/images/dot2.png);
+                  background-size: cover;
+                  width: 33pr;
+                  height: 33pr;
+                }
+                .line {
+                  margin: 12pr 2pr 12pr 2pr;
+                  width: 209pr;
+                  height: 4pr;
+                  border: 4pr dashed #313131;
+                  width: 200pr;
                 }
               }
-              .bottom {
-                .progress-desc {}
+              .progress-bottom {
+                font-size: 28pr;
+                color: #FFC4D1;
+                .desc-active {
+                  color: #ffffff;
+                }
+                .desc1 {
+                  margin-right: 100pr;
+                }
+                .desc2 {
+                  margin-right: 76pr;
+                }
+                .desc3 {
+                  margin-right: 43pr;
+                }
               }
             }
           }
