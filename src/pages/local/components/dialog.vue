1a5176b5e81fb0c96634f379efe3f9ff52e7b4c3
ly <yue.li@tiejin.me>
2018-08-27 15:32:07 +0800

    更改排行榜

--- src/pages/local/components/dialog.vue
+++ src/pages/local/components/dialog.vue
@@ -11,6 +11,8 @@
 </template>
 
 <script>
+
+  import { mapState } from 'vuex';
   export default {
     name: 'localDialog',
     props:{
@@ -18,13 +20,24 @@
       share: Boolean,
       content: String
     },
+    computed: {
+      ...mapState(['IS_APP'])
+    },
     methods:{
       handleClose() {
         this.$emit('close');
       },
       handleClick() {
         if (this.share) {
-
+          if (this.IS_APP) {
+            // 端内跳分享页
+            this.$router.push({
+              name: 'localShare'
+            })
+          } else if (this.IS_WX) {
+            // 微信端调sdk
+            
+          }
         } else {
           this.handleClose();
         }
