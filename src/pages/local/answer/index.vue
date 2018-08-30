db36988671ab6785e7bd0eb9f0997b16f0d94975
jiax <jia.xu@tiejin.me>
2018-08-30 11:07:47 +0800

    去掉答题页动画

--- src/pages/local/answer/index.vue
+++ src/pages/local/answer/index.vue
@@ -6,12 +6,12 @@
       <div class="hd-img"></div>
       <div class="answer-box">
         <div class="answer-acount">{{currentQuesitionNum + 1}}/8</div>
-        <div class="subject animated slideInUp">{{startData[currentQuesitionNum].title}}</div>
-        <div class="optoins animated slideInUp" v-for="(item, index) in startData[currentQuesitionNum].answers" :key="index">
+        <div class="subject">{{startData[currentQuesitionNum].title}}</div>
+        <div class="optoins" v-for="(item, index) in startData[currentQuesitionNum].answers" :key="index">
           <div class="list" :data-index="index" :data-seq="item.seq" :data-questionId="item.questionId" :class="index===checkNum ? 'bg-yellow' : ''" @click="checkOptions($event)">{{item.title}}</div>
         </div>
       </div>
-      <div class="next-box" :class="isUpdate ? 'animated slideInUp' : ''">
+      <div class="next-box">
         <div :class="isCheck ? 'checked' : 'next-btn'" v-if="currentQuesitionNum < 7" @click="next(startResult.userAnswerId)"></div>
         <div class="finish" :class="isCheck ? 'isfinish' : 'unfinish'" v-if="currentQuesitionNum == 7" @click="next(startResult.userAnswerId)">完成答题</div>
       </div>
