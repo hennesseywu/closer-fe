31c5be328c661b05af83155e6a4288ec74119cfb
hennessey <yuhang.wu@tiejin.com>
2018-09-01 11:17:34 +0800

    排查问题11

--- src/pages/local/share/index.vue
+++ src/pages/local/share/index.vue
@@ -9,13 +9,13 @@
               <img :src="makeFileUrl(user.avatar)" class="share-user-avatar" crossOrigin="Anonymous">
               <div :class="'share-user-filter '+levelData.logo"></div>
             </div>
-            <!-- <div class="share-user-name">{{user.fullname}}</div>
-            <div class="share-desc">
+             <div class="share-user-name">{{user.fullname}}</div>
+            <!-- <div class="share-desc">
               在【谁是成都最土著】中获得
               <span class="share-desc-score">{{score}}</span> 分，
               <br/>
               <span class="share-desc-tip">{{levelData.tip}}</span>
-            </div> -->
+            </div> --> -->
             <!-- <div class="share-title box box-lr box-center-center">
               <div class="line left"></div>
               <div class="name">获得称号</div>
