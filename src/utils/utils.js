14dffadf3424912bffc74cdf0a0def7da2ba66fe
hennessey <yuhang.wu@tiejin.com>
2018-09-01 20:58:29 +0800

    排查问题30

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -261,7 +261,7 @@ function convertToImage(canvas, width, height, type) {
   function getDataURL(canvas, type, width, height) {
     canvas = scaleCanvas(canvas, width, height);
     console.log(3, canvas);
-    let dataUrl = canvas.toDataURL(type, 0.3);
+    let dataUrl = canvas.toDataURL(type, 0.1);
     console.log(4);
     return dataUrl
   }
