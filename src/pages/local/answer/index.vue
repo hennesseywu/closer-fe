0c511ffbb8f47da0ccfeeeb4329892a18ac01efb
hennessey <yuhang.wu@tiejin.com>
2018-09-03 11:13:36 +0800

    修改答题动画bug

--- src/pages/local/answer/index.vue
+++ src/pages/local/answer/index.vue
@@ -156,7 +156,7 @@
           this.isUpdate=false;
           setTimeout(() => {
               this.isUpdate = true
-          }, 50);
+          }, 5);
           this.checkNum = ''
           this.isCheck = false
           this.questionNum++
