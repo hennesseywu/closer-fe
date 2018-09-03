dc589e5673287162705d45d3a8383084d7fc676d
hennessey <yuhang.wu@tiejin.com>
2018-09-03 18:08:00 +0800

    还原share页

--- src/pages/local/share/index.vue
+++ src/pages/local/share/index.vue
@@ -1,163 +1,116 @@
 <template>
-  <div class="main local-share" :class="{'in-app': IS_APP}">
-    <local-header v-if="IS_APP" back home></local-header>
-    <div class="share-wrapper">
-      <div class="share-container">
-        <div ref="canvasContainer" class="share-box">
-          <div v-if="answerId" class="share-score">
-            <div class="share-user-img">
-              <img :src="makeFileUrl(user.avatar)" class="share-user-avatar" crossOrigin="Anonymous">
-              <div class="share-user-filter">
-                <img :src="levelData.logoImg" alt="">
-              </div>
-            </div>
-             <div class="share-user-name">{{user.fullname}}</div>
-            <div class="share-desc">
-              在【谁是成都最土著】中获得
-              <span class="share-desc-score"> {{score}}</span> 分，
-              <br/>
-              <span class="share-desc-tip">{{levelData.tip}}</span>
-            </div> 
-            <div class="share-title box box-lr box-center-center">
-              <div class="line left"></div>
-              <div class="name">获得称号</div>
-              <div class="line right"></div>
-            </div>
-            <div class="share-tag">
-              <img :src="levelData.tagImg" alt="">
-            </div>
-             <div class="share-qrcode"> 
-              <qrcode-vue :value="qrcode.val" :size="qrcode.size"></qrcode-vue>
-            </div>
-            <div class="share-tip">长按识别二维码参与游戏，和他Pk吧</div>
-          </div>
-          <div v-else class="share-default">
-            <img :src="defaultImg" alt="" class="share-default-bg">
-            <div class="share-qrcode">
-              <qrcode-vue :value="qrcode.val" :size="qrcode.size"></qrcode-vue>
-            </div>
-          </div>
-          <img class="share-img" id="share-img" src="">
-        </div>
+  <div class="share-wrapper" >
+    <div class="is-app" v-if="isApp">
+      <div class="share-img">
+        <img :src="shareData" />
       </div>
-      <div v-if="IS_APP" class="share-items box box-lr box-center-center">
-        <div class="item item1 box box-tb box-center-center" @click="toShare('inviteNewGuyActionWeChat', shareData)">
+      <div class="share-items box box-lr box-center-center">
+        <div class="item item1 box box-tb box-center-center" @click="toShare('inviteNewGuyActionWeChat')">
           <span class="weixin"></span>
           <span>好友</span>
         </div>
-        <div class="item item2 box box-tb box-center-center" @click="toShare('inviteNewGuyActionWxTimeLine', shareData)">
+        <div class="item item2 box box-tb box-center-center" @click="toShare('inviteNewGuyActionWxTimeLine')">
           <span class="pyq"></span>
           <span>朋友圈</span>
         </div>
-        <div class="item item3 box box-tb box-center-center" @click="toShare('inviteNewGuyActionSavePicture', shareData)">
+        <div class="item item3 box box-tb box-center-center" @click="toShare('inviteNewGuyActionSavePicture')">
           <span class="download"></span>
           <span>保存至相册</span>
         </div>
       </div>
+    </div>
+    <div class="is-weixin" v-else>
+      <img :src="shareData" />
     </div>
   </div>
 </template>
 
 <script>
   import {
-    Toast,
-    Indicator
+    Toast
   } from 'mint-ui';
   import {
     mapState,
     mapActions
   } from "vuex";
-  import localHeader from '../components/header';
-  import {
-    makeFileUrl,
-    addParamsForUrl,
-    html2Image,
-    tjUploadFile
-  } from '../../../utils/utils'
-  import QrcodeVue from 'qrcode.vue';
-  import defaultImg from '../assets/images/default_share.png';
   export default {
     data() {
       return {
         isApp: this.$store.state.IS_APP,
-        isLogin: false,
-        qrcode: {
-          val: 'https://a.tiejin.cn/local',
-          size: 80
-        },
-        showData: [{
-          logoImg: require('../assets/images/avatar1.png'),
-          tip: '赢得5元红包！',
-          tagImg: require('../assets/images/local1.png')
-        }, {
-          logoImg: require('../assets/images/avatar2.png'),
-          tip: '赢得2元现金红包，全答对可得5元哦！',
-          tagImg: require('../assets/images/local2.png')
-        }, {
-          logoImg: require('../assets/images/avatar3.png'),
-          tip: '和5元现金红包失之交臂，你要来试试吗？',
-          tagImg: require('../assets/images/local3.png')
-        }],
-        defaultImg: defaultImg,
-        imgUrl: ''
+        isLogin: false
       }
-    },
-    components: {
-      localHeader,
-      QrcodeVue
     },
     created() {
-      Indicator.open();
       console.log('isAPP', this.isApp)
       // this.userShare()
-      if (this.IS_WX) {
-        console.log('share wxshare--')
-        this.initWxConfig()
-      }
-      if (this.IS_DEV) {
-        this.qrcode.val = 'https://a-sandbox.tiejin.cn/local?activityId=' + this.activityId + '&inviter=' + this.objectID + '&salt=' + this.salt
-      } else {
-        this.qrcode.val = 'https://a.tiejin.cn/local?activityId=' + this.activityId + '&inviter=' + this.objectID + '&salt=' + this.salt
-      }
     },
     computed: {
-      ...mapState(['IS_DEV', 'IS_APP', 'IS_WX']),
       ...mapState('local', {
-        objectID: state => state.user.objectID || '',
-        salt: state => state.statistic.signSalt,
-        activityId: state => state.activityId,
-        user: state => state.user,
-        answerId: state => state.endData.userAnswerId,
-        shareData: state => state.shareData,
-        level: state => state.endData.level,
-        score: state => state.endData.score
-      }),
-      levelData() {
-        return this.showData[parseInt(this.level) - 1]
-      }
+        shareData: state => state.shareData
+      })
     },
-    mounted() {
-      console.log('answerId:', this.answerId)
-      setTimeout(this.drawHtmlToCanvas, 100)
+    async mounted() {
+      if (!this.isApp) {
+        let wxConfig = await this.wechatConfig();
+        console.log('wxConfig', wxConfig)
+        if (wxConfig && wxConfig.signature && wxConfig.appId && wxConfig.nonceStr && wxConfig.timestamp) {
+          wx.config({
+            "debug": this.$store.state.IS_DEV, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            "signature": wxConfig.signature,
+            "appId": wxConfig.appId,
+            "nonceStr": wxConfig.nonceStr,
+            "timestamp": wxConfig.timestamp,
+            jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表
+          });
+          wx.ready(function() {
+            console.log("ready")
+            // 分享朋友
+            wx.onMenuShareAppMessage({
+              title: '是成都人就来瓜分百万大奖', // 分享标题
+              desc: '参与成都人纯度测试，纯度越高，奖金越多！', // 分享描述
+              link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+              imgUrl: '', // 分享图标
+              type: '', // 分享类型,music、video或link，不填默认为link
+              dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+              success: function() {
+                // 用户点击了分享后执行的回调函数
+              }
+            })
+  
+            // 分享朋友圈
+            wx.onMenuShareTimeline({
+              title: '是成都人就来瓜分百万大奖', // 分享标题
+              link: 'location.href', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+              imgUrl: '', // 分享图标
+              success: function() {
+                // 用户点击了分享后执行的回调函数
+              }
+            })
+            // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+          })
+          wx.error(function(res) {
+            console.log("error", res)
+            // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+          });
+        }
+      }
     },
     methods: {
       ...mapActions("local", [
         "wechatConfig",
         "userShare",
-        "checkLoginInApp",
-        "initWxConfig"
+        "checkLoginInApp"
       ]),
       toShare(type) {
         let ua = this.$store.state.UA
-        let url = this.imgUrl;
-        console.log('share--', type, url, ua)
+        console.log('share--', ua)
         if (ua.indexOf("closer-ios") > -1) {
           setupWebViewJavascriptBridge(function(bridge) {
             console.log("toShare ios bridge", bridge)
             if (bridge) {
               bridge.callHandler("inviteNewerAction", {
                 "type": type,
-                "img": url
+                "img": this.shareData
               }, function(data, responseCallback) {})
             }
           })
@@ -165,48 +118,21 @@
           console.log("toShare android bridge", bridge)
           if (typeof window.bridge != "undefined") {
             try {
-              window.bridge.inviteNewerAction(JSON.stringify({
+              window.bridge.inviteNewerAction({
                 "type": type,
-                "img": url
-              }));
+                "img": this.shareData
+              });
             } catch (e) {
               Toast("请升级最新版本客户端")
             }
           }
         }
-      },
-      makeFileUrl(url) {
-        let avatar = makeFileUrl(url)
-        return avatar
-      },
-      drawHtmlToCanvas() {
-        let self = this;
-        let container = self.$refs.canvasContainer;
-        html2Image(container).then(img => {
-          // img.setAttribute('class', 'qr-img');
-          // img.setAttribute("crossOrigin", 'Anonymous')
-          // document.getElementById("share-img").src=img.src;
-          console.log('html2Image-finish')
-          // container.appendChild(img);
-            Indicator.close();
-          // if (self.IS_APP) {
-            tjUploadFile(img).then(({
-              data
-            }) => {
-
-              self.imgUrl = self.makeFileUrl(data.result.url);
-               document.getElementById("share-img").src=self.imgUrl;
-
-            })
-          // }
-        })
       }
     }
   }
 </script>
 
-<style lang="less">
-  @import '../assets/style/main.less';
+<style lang="less" scoped>
   @import '../assets/style/share.less';
 </style>
 
