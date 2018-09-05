8fde233a491cff13fe1623489d5e2367871e6717
ly <yue.li@tiejin.me>
2018-09-05 17:19:50 +0800

    测试ios9的问题

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import 'babel-polyfill'
 // import './reset.css'
 import './assets/style/reset.less'
 import './assets/style/common.css'
--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,10 @@ module.exports = {
   devServer: {
     host: 'local.tiejin.cn',
     port: 8889
+  },
+  configureWebpack: {
+    entry: [
+        'babel-polyfill', './src/main.js'
+    ]
   }
 }
