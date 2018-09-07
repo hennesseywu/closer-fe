bc965234ef3c427d7981a56186fd6666adb55bb9
ly <yue.li@tiejin.me>
2018-09-07 15:23:46 +0800

    test hash router

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,12 +6,14 @@ export default {
     subdevserverUrl: 'http://oms-sandbox.tiejin.cn/command',
     serverDevUrl: 'https://api-sandbox.tiejin.cn/command/',
     downUrl: 'http://a.app.qq.com/o/simple.jsp?pkgname=com.ums.closer',
+    wxRedirectDevUrl: 'https://h5-sandbox.tiejin.cn/redirect?redirectUrl=',
+    wxRedirectUrl: 'https://h5.tiejin.cn/redirect?redirectUrl=',
     // 本地测试微信二跳链接
-    wxLoginLocalUrl: "https://h5-sandbox.tiejin.cn/redirect?redirectUrl=http://local.tiejin.cn:8889",
+    wxLoginLocalUrl: "http://local.tiejin.cn:8889/#",
     // sandbox测试微信二跳链接
-    wxLoginDevUrl: "https://h5-sandbox.tiejin.cn/redirect?redirectUrl=https://a-sandbox.tiejin.cn",
+    wxLoginDevUrl: "https://a-sandbox.tiejin.cn/#",
     // 线上微信二跳链接
-    wxLoginUrl: "https://h5.tiejin.cn/redirect?redirectUrl=https://a.tiejin.cn",
+    wxLoginUrl: "https://a.tiejin.cn/#",
     file: '/file/upload/public',
     admin: {
         // 微信授权获取code
